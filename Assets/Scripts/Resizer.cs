f9a5c149549f5a922478c68759b753bcc63568dc
DoeCommaJohn <92694497+DoeCommaJohn@users.noreply.github.com>
2021-10-17 16:45:40 -0500

    Add files via upload

---
+++ Assets/Scripts/Resizer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resizer : ContentSizeFitter
+{
+    public UIMaster UIM = null;
+
+    protected override void Start()
+    {
+        UIM = transform.parent.parent.parent.parent.parent.GetComponent<UIMaster>();
+    }
+    public override void SetLayoutHorizontal()
+    {
+        base.SetLayoutHorizontal();
+        if (UIM != null)
+            UIM.SetTextWidth(this.GetComponent<RectTransform>().sizeDelta.x);
+    }
+}
